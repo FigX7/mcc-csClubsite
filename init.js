e76344c950eea0e212d2c5313782f52ca7465731
Figx7 <michael.figueroa73@gmail.com>
2017-10-18 21:10:55 -0700

    Sign Form Showing Properly
    -FIXED layout

--- init.js
+++ init.js
@@ -14,13 +14,7 @@ window.onload = () => {
 }
 
 
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
+
 
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
@@ -42,3 +36,121 @@ function toggleFunction() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+//<-------------- lOGIN fUNCTIONS
+document.getElementById("loginBtn").onclick = function() {myHide()};
+
+function myHide() {
+  var x =  document.getElementById("cont_centrar");
+  
+}
+
+function sign_up(){
+	  var inputs = document.querySelectorAll('.input_form_sign');
+	document.querySelectorAll('.ul_tabs > li')[0].className=""; 
+	document.querySelectorAll('.ul_tabs > li')[1].className="active"; 
+	  
+	  for(var i = 0; i < inputs.length ; i++  ) {
+	if(i == 2  ){
+
+	}else{  
+	document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+	}
+	} 
+
+	setTimeout( function(){
+	for(var d = 0; d < inputs.length ; d++  ) {
+	 document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block active_inp";  
+	   }
+
+
+	 },100 );
+	 document.querySelector('.link_forgot_pass').style.opacity = "0";
+	   document.querySelector('.link_forgot_pass').style.top = "-5px";
+	   document.querySelector('.btn_sign').innerHTML = "SIGN UP";    
+	  setTimeout(function(){
+
+	 document.querySelector('.terms_and_cons').style.opacity = "1";
+	  document.querySelector('.terms_and_cons').style.top = "5px";
+	 
+	  },500);
+	  setTimeout(function(){
+	    document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_none";
+	 document.querySelector('.terms_and_cons').className = "terms_and_cons d_block";
+	  },450);
+
+	}
+
+
+
+	function sign_in(){
+	  var inputs = document.querySelectorAll('.input_form_sign');
+	document.querySelectorAll('.ul_tabs > li')[0].className = "active"; 
+	document.querySelectorAll('.ul_tabs > li')[1].className = ""; 
+	  
+	  for(var i = 0; i < inputs.length ; i++  ) {
+	switch(i) {
+	    case 1:
+	 console.log(inputs[i].name);
+	        break;
+	    case 2:
+	 console.log(inputs[i].name);
+	    default: 
+	document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+	}
+	} 
+
+	setTimeout( function(){
+	for(var d = 0; d < inputs.length ; d++  ) {
+	switch(d) {
+	    case 1:
+	 console.log(inputs[d].name);
+	        break;
+	    case 2:
+	 console.log(inputs[d].name);
+
+	    default:
+	 document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block";  
+	 document.querySelectorAll('.input_form_sign')[2].className = "input_form_sign d_block active_inp";  
+
+	   }
+	  }
+	 },100 );
+
+	 document.querySelector('.terms_and_cons').style.opacity = "0";
+	  document.querySelector('.terms_and_cons').style.top = "-5px";
+
+	  setTimeout(function(){
+	 document.querySelector('.terms_and_cons').className = "terms_and_cons d_none"; 
+	document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
+
+	 },500);
+
+	  setTimeout(function(){
+
+	 document.querySelector('.link_forgot_pass').style.opacity = "1";
+	   document.querySelector('.link_forgot_pass').style.top = "5px";
+	    
+
+	for(var d = 0; d < inputs.length ; d++  ) {
+
+	switch(d) {
+	    case 1:
+	 console.log(inputs[d].name);
+	        break;
+	    case 2:
+	 console.log(inputs[d].name);
+
+	         break;
+	    default:
+	 document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign";  
+	}
+	  }
+	   },1500);
+	   document.querySelector('.btn_sign').innerHTML = "SIGN IN";    
+	}
+
+
+	window.onload =function(){
+	  document.querySelector('.cont_centrar').className = "cont_centrar cent_active";
+
+	}
